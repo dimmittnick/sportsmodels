7e08f068908a5ad3cbbe752c62a0f081ecbcaa58
nicholasdimmitt <nicholasdimmitt@gmail.com>
2022-12-19 15:43:24 -0500

    initializing structure

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # sportsmodels
 
 Repository for sportsmodels.org. This website will leverage machine learning and datasets that update daily to allow users to construct their own ML models and create daily predictions to support them in fantasy, sports betting, or pure interest. Will give use the ability to select their target variable and what features they want to use then will spit out daily predictions. Will eventually scale into all sports and longer time frames that don't necesarilly do daily predictions. The first model is an nhl daily predictions model.
+
+
+12-16-2022
+
+Need to get familiar with microsoft azure to house the data. Need to create a way that will update the database everyday. have the code to do it but dont know the link between the database and bulk uploading csv files.
--- nhl/database/tables.py
+++ nhl/database/tables.py
@@ -26,7 +26,7 @@ else:
   cursor = conn.cursor()
 
 # create table
-cursor.execute("CREATE TABLE IF NOT EXISTS skaters(assists INT, evGoals INT, evPoints INT, faceoff_win_pct FLOAT, game_date VARCHAR(10), game_id VARCHAR(12), game_winning_goals INT, games_played INT, goals INT, home_road CHAR, last_name VARCHAR(24), opponent_team_abbrev VARCHAR(5), ot_goals INT, penalty_minutes INT, player_id INT, plus_minus INT, points INT, points_per_game FLOAT, position_code TEXT, pp_goals INT, pp_points INT, sh_goals INT, sh_points INT, shooting_pct FLOAT, shoots_catches TEXT, shots INT, skater_full_name VARCHAR(64), team_abbrev VARCHAR(3), time_on_ice_per_game FLOAT)")
+cursor.execute("CREATE TABLE skaters(assists INT, ev_goals INT, ev_points INT, faceoff_win_pct FLOAT, game_date DATE, game_id INT, game_winning_goals INT, games_played INT, goals INT, home_road CHAR, last_name VARCHAR(24), opponent_team_abbrev VARCHAR(5), ot_goals INT, penalty_minutes INT, player_id INT, plus_minus INT, points INT, points_per_game FLOAT, position_code CHAR, pp_goals INT, pp_points INT, sh_goals INT, sh_points INT, shooting_pct FLOAT, shoots_catches CHAR, shots INT, skater_full_name VARCHAR(64), team_abbrev VARCHAR(3), toi FLOAT)")
 
 
 # cleanup
