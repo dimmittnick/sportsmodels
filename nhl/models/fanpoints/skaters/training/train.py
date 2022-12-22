52dda83a568f3885ff68b83b607b112e436ac7df
nicholasdimmitt <nicholasdimmitt@gmail.com>
2022-12-22 12:49:21 -0600

    complete model 1.1

--- nhl/models/fanpoints/skaters/training/train.py
+++ nhl/models/fanpoints/skaters/training/train.py
@@ -1,2 +1,25 @@
 import pandas as pd
 import numpy as np
+import data, data_prep, training
+
+goalie_list = ['savePct']
+skater_list = ['goals', 'shots', 'timeOnIce', 'ppTimeOnIce', 'fanPoints', 'points']
+team_list = ['goalsAgainst', 'shotsAgainstPerGame']
+
+df = data.main()
+df = data_prep.main(df, goalie_list=goalie_list, skater_list=skater_list, team_list=team_list, per_sixty_list=None)
+
+print(df.columns)
+
+cat_var = input("Categorical Variables: ")
+
+edf = training.ohe(df, cat_vars=cat_var)
+
+print(edf.columns)
+
+features = input("Feature List: ")
+target = input("Target Variable: ")
+
+training.trainer(df=edf, features=features, target=target, learning_rate=0.1, n_estimators=1500, max_depth=5, scale=True,save_model=False)
+
+
