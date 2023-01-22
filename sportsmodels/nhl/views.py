ee98741377047c5eb250419d22ea27a535aff8f1
nicholasdimmitt <nicholasdimmitt@gmail.com>
2023-01-22 09:56:33 -0500

    filter database by date and sort by fanpoints

--- sportsmodels/nhl/views.py
+++ sportsmodels/nhl/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import NhlPreds
+from datetime import *
+
+today = datetime.today().strftime("%Y-%m-%d")
 
 def index(request):
-    return render(request, "nhl/index.html", {
-        "preds": NhlPreds.objects.all()
-    })
+    allpreds = NhlPreds.objects.filter(date=today)
+    sorted_preds = sorted(allpreds, key=lambda x:x.prediction, reverse=True)
+    context = {'allpreds':allpreds,
+               'sorted_preds':sorted_preds}
+    return render(request, "nhl/index.html", context)
 
